fb9b1000093b47ae5f724d41c84ee3583402d64f
Simon Cossar <scossar3@gmail.com>
2015-03-14 15:52:49 -0700

    get ready

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,17 +8,15 @@ set :stage, :staging
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
+server '104.236.115.127', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(~/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+}
 
 fetch(:default_env).merge!(wp_env: :staging)
 
