426b4aadb44ceca5d0b2744cab14a66eb59a7655
Simon Cossar <scossar3@gmail.com>
2015-03-16 15:16:15 -0700

    clean up logo

--- web/app/themes/bww/templates/header.php
+++ web/app/themes/bww/templates/header.php
@@ -5,7 +5,7 @@
 		<div class="row">
 			<div class="site-logo">
 				<a href="<?php echo esc_url(home_url('/')); ?>">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_1.png"
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bww-logo.png"
 					     alt="Books With Wings"/>
 				</a>
 			</div>
