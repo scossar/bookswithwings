b6e6bd23c3aa1efa8967e45150b11e2bbb6156b9
Simon Cossar <scossar3@gmail.com>
2015-03-16 11:52:59 -0700

    clean up styles

--- web/app/themes/bww/templates/menu-donate.php
+++
@@ -1,9 +0,0 @@
-<?php use Roots\Sage\Nav; ?>
-<nav class="volunteer-menu" role="navigation">
-	<?php
-	if (has_nav_menu('donation_navigation')) :
-		wp_nav_menu(['theme_location' => 'donation_navigation',
-		             'walker' => new Nav\SageNavWalker(), 'menu_class' => 'nav nav-pills nav-justified']);
-	endif;
-	?>
-</nav>
