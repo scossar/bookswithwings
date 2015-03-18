eef519209dcf76fa0a0942122177c579f00bce95
Simon Cossar <scossar3@gmail.com>
2015-03-17 20:06:15 -0700

    add class to metadata

--- web/app/themes/bww/templates/content.php
+++ web/app/themes/bww/templates/content.php
@@ -2,7 +2,9 @@
   <header>
 	  <?php // For the moment we will not have the title link to the article - we'll just display it here until we get more posts. ?>
     <h2 class="entry-title"><?php the_title(); ?></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
+	  <div class="post-metadata">
+	    <?php get_template_part('templates/entry-meta'); ?>
+	  </div>
   </header>
   <div class="entry-content">
     <?php the_content(); ?>
