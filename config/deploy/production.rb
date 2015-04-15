9e7068ee27188c4ed0fb6443b2595198bb34bcbd
Simon Cossar <scossar3@gmail.com>
2015-04-15 16:09:30 -0700

    update domain name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ set :stage, :production
 
 # Extended Server Syntax
 # ======================
-server '104.236.246.227', user: 'deploy', roles: %w{web app db}
+server 'bookswithwings.org', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
